b30926c8e427d498edad99ffb2c5c3bb7c4e65f6
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-10 13:37:33 -0500

    feat: set up vue application

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Navbar from "./components/NavBar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<template>
+  <Navbar></Navbar>
+</template>
+
+<style></style>
