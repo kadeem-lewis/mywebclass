9728b66677a559591024897cfd53811b43b21659
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-10 13:36:15 -0500

    feat: replace webpack bundler with vite

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: "public_html",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+});
